9159a8c9606590c38cabe3edf24a545771b4a44b
kouaouci <karimakouaouci2803@gmail.com>
2021-12-10 10:43:49 +0100

    quete fini

--- TestDate.java
+++ TestDate.java
@@ -1,3 +1,4 @@
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -18,6 +19,23 @@ class TestDate {
         sc.close();
 
         // TODO : your code after this line
+     // format to retreive userDate
+     		SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+
+     		try {
+     			Date date = sdf.parse(month + "-" + day + "-" + year);
+     			System.out.println("The date is: " + sdf.format(date));
+
+     		} catch (ParseException e) {
+     			e.printStackTrace();
+     		}
+
+     		//retreive the day of the week 
+     		GregorianCalendar userCalendar = new GregorianCalendar(year, month - 1, day);
+     		int dayOfWeek = userCalendar.get(Calendar.DAY_OF_WEEK);
+     		System.out.println("The day of the week is: " + dayOfWeek);
+
+        
         
     }
 }
